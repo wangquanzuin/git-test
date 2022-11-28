a10070815e612b1a75c100896a7cd92d8f201d63
wq <2207383482@qq.com>
2022-11-28 11:16:27 +0800

    push commit second

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot_fix test!");
         System.out.println("push test!");
+        System.out.println("push test2!");
     }
 }
